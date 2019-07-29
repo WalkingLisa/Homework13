117c711573c18b8369a2cc5fd4cdc80a10e5371a
Lisa <51434001+WalkingLisa@users.noreply.github.com>
2019-07-29 19:12:02 +0200

    Add files via upload

--- Basketball_and_Football.py
+++ Basketball_and_Football.py
@@ -1,49 +1,51 @@
-#Parent-Class Player
-class Player():
-    def __init__(self, first_name, last_name, height_cm, weight_kg):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.height_cm = height_cm
-        self.weight_kg = weight_kg
+#coding=utf8
 
-#Parent-Methode Pounds
-    def weight_to_lbs(self):
-        pounds = self.weight_kg * 2.20462262
-        return pounds
+from sport_class import Player
+from sport_class import FootballPlayer
+from sport_class import BasketballPlayer
 
-#Child-Class BasketballPlayer
-class BasketballPlayer(Player):
-    def __init__(self, first_name, last_name, height_cm, weight_kg, points, rebounds, assists):
-        super().__init__(first_name=first_name, last_name=last_name, height_cm=height_cm, weight_kg=weight_kg)
-        self.points = points
-        self.rebounds = rebounds
-        self.assists = assists
+if __name__ == '__main__':
+
+    print("Create your own player!")
 
 
-#Child-Class FootballPlayer
-class FootballPlayer(Player):
-    def __init__(self, first_name, last_name, height_cm, weight_kg, goals, yellow_cards, red_cards):
-        super().__init__(first_name=first_name, last_name=last_name, height_cm=height_cm, weight_kg=weight_kg)
-        self.goals = goals
-        self.yellow_cards = yellow_cards
-        self.red_cards = red_cards
+    while True:
+        choice=input("Which player do you want to create? Basketball or Football?: ")
+        if choice=="Football":
+            f_name = input("Enter player's first name: ")
+            l_name = input("Enter player's last name: ")
+            height = input("Enter player's height: ")
+            weight = input("Enter player's weight: ")
+            goals = input("Enter the number of player's goals: ")
+            y_cards = input("Enter the number of player's yellow cards: ")
+            r_cards = input("Enter the number of player's red cards: ")
+            new_player_foot = FootballPlayer(first_name=f_name, last_name=l_name, height_cm=float(height), weight_kg=float(weight),goals=int(goals), yellow_cards=int(y_cards), red_cards=int(r_cards))
+            try:
+                with open("football_players.txt", "w") as football_file:
+                    football_file.write(str(new_player_foot.__dict__))
+                print("Player successfully entered!")
+                print("Player's data: {0}".format(new_player_foot.__dict__))
+            except:
+                raise FileNotFoundError
 
 
-print("Create your own player!")
 
-f_name = input("Enter player's first name: ")
-l_name = input("Enter player's last name: ")
-height = input("Enter player's height: ")
-weight = input("Enter player's weight: ")
-goals = input("Enter the number of player's goals: ")
-y_cards = input("Enter the number of player's yellow cards: ")
-r_cards = input("Enter the number of player's red cards: ")
+        elif choice=="Basketball":
+            f_name = input("Enter player's first name: ")
+            l_name = input("Enter player's last name: ")
+            height = input("Enter player's height: ")
+            weight = input("Enter player's weight: ")
+            points = input("Enter player's points: ")
+            rebounds = input("Enter player's rebounds: ")
+            assists = input("Enter player's assists: ")
+            new_player_basket = BasketballPlayer(first_name=f_name, last_name=l_name, height_cm=float(height), weight_kg=float(weight), points=int(points), assists=int(assists), rebounds=int(rebounds))
+            try:
+                with open("basketball_players.txt", "w") as basketball_file:
+                    basketball_file.write(str(new_player_basket.__dict__))
+                print("Player successfully entered!")
+                print("Player's data: {0}".format(new_player_basket.__dict__))
+            except:
+                raise FileNotFoundError
 
-new_player = FootballPlayer(first_name=f_name, last_name=l_name, height_cm=float(height), weight_kg=float(weight),
-                            goals=int(goals), yellow_cards=int(y_cards), red_cards=int(r_cards))
 
-with open("football_players.txt", "w") as football_file:
-    football_file.write(str(new_player.__dict__))
 
-print("Player successfully entered!")
-print("Player's data: {0}".format(new_player.__dict__))
